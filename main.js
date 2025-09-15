7666de9602523358a6a347ee1948625dca2d3fe0
ahmed <ahsan.elm.39@gmail.com>
2025-09-15 12:49:26 +0300

    update

--- main.js
+++ main.js
@@ -20,3 +20,34 @@ moveUp.onclick=function(){
     window.scrollTo(0,0)
 }
 
+//////////////////
+
+
+const mainLinks = document.querySelectorAll('.main-links li a')
+mainLinks.forEach(link => {
+  link.addEventListener('click', ()=> {
+    mainLinks.forEach(l => l.classList.remove('active'))
+    e.currentTarget.classList.add('active')
+  })
+})
+
+const numbersSection = document.querySelector('.data')
+const numbers = document.querySelectorAll('.data .box div span:nth-child(1)')
+let started = false;
+
+window.onscroll= ()=> {
+        if(window.scrollY >= numbersSection.offsetTop - 300){
+            if(!started){
+                numbers.forEach(num => {
+                    const goal = num.dataset.set
+                    let count = setInterval(() => {
+                        num.textContent++
+                        if (num.textContent == goal) {
+                            clearInterval(count)
+                        }
+                    },1/goal);
+                });
+            }
+        started = true;
+    }
+}
